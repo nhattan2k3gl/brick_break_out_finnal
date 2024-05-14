91dd6a2bc7411c51ce2d31ed902fee5a44d7910a
DoanNhatTan <99519033+DoanNhatTan@users.noreply.github.com>
2024-05-14 12:30:21 +0700

    bg_breakout

--- breakout.js
+++ breakout.js
@@ -1,1 +1,16 @@
-console.log("chao cau")
+let ctx;
+/// board
+let blackBoard = {
+    height: 500,
+    with: 500
+}
+//function draw board
+function draw_board(){
+    let board = document.getElementById("board");
+    board.height = 500;
+    board.width = 500;
+    ctx = board.getContext("2d");
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(10, 0, 500, 500)
+}
+draw_board();
